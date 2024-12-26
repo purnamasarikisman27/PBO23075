99ebe9f7afd3556af1ade89491b3aef79096e4d9
purnamasarikisman27 <sarikisman27@gmail.com>
2024-12-26 12:57:00 +0900

    Add files via upload

---
+++ kode_modul_23075.py
@@ -0,0 +1,12 @@
+import contoh_modul_23075
+angka1 = 10
+angka2 = 5
+print("hasil operasi:")
+print(f"penjumlahan:{contoh_modul_23075.tambah(angka1, angka2)}")
+print(f"pengurangan:{contoh_modul_23075.kurang(angka1, angka2)}")
+print(f"perkalian:{contoh_modul_23075.kali(angka1, angka2)}")
+print(f"pembagian:{contoh_modul_23075.bagi(angka1, angka2)}")
+angka3 = 20
+angka4 = 0
+print("\nhasil operasi dengan pembagian nol:")
+print(f"pembagian:{contoh_modul_23075.bagi(angka3, angka4)}")
